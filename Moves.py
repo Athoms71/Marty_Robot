f0297370adda2c1ae7c7e6b1620d4c8cc6f698df
robin-r <robinrus42@gmail.com>
2025-05-22 12:00:35 +0200

    commentaire des fonctions dans moves et optimisation de calibration_path

--- Moves.py
+++ Moves.py
@@ -7,15 +7,26 @@ class Moves:
         self.marty = marty
 
     def walkcase(self, case: int = 1, side: str = "forward"):
+        """
+            :param case: Nombre de cases à parcourir. Par défaut, 1.
+            :param side: Direction ("forward" ou "backward"). Par défaut, "forward".
+        """
         if(side == "forward"):
             self.marty.walk(num_steps=case*7, start_foot='auto', turn=0, step_length=25, move_time=1500, blocking=None)
         else:
             self.marty.walk(num_steps=case * 7, start_foot='auto', turn=0, step_length=-25, move_time=1500, blocking=None)
 
     def sidecase(self, case: int = 1, side: str = "right"):
+        """
+            :param case: Nombre de cases à parcourir. Par défaut, 1.
+            :param side: Direction ("right" ou "left"). Par défaut, "right".
+        """
         self.marty.sidestep(side=side, steps=case*6, step_length=35, move_time=1000, blocking=True)
 
     def circletime(self, time: int = 1):
+        """
+            :param time: Nombre de répétitions de la danse circulaire. Par défaut, 1.
+        """
         for i in range(time):
             self.marty.arms(left_angle=0, right_angle=135, move_time=500, blocking=False)
             self.marty.circle_dance(side="right", move_time=1000, blocking=False)
@@ -23,6 +34,9 @@ class Moves:
             self.marty.circle_dance(side="left", move_time=1000, blocking=False)
 
     def turn(self, side: str = "left"):
+        """
+            :param side: Direction du tour ("left" pour gauche ou "right" pour droite). Par défaut, "left".
+        """
         if side == "left":
             self.marty.arms(left_angle=0, right_angle=135, move_time=500, blocking=False)
             for i in range(2):
@@ -45,12 +59,15 @@ class Moves:
             self.marty.arms(left_angle=0, right_angle=0, move_time=500, blocking=False)
 
     def calibration_path(self):
-            self.walkcase(2)
-            self.turn()
-            self.walkcase(1)
-            self.turn()
-            self.walkcase(2)
-            self.turn("right")
-            self.walkcase(1)
-            self.turn("right")
-            self.walkcase(2)
+        path = [
+            (2, "left"),
+            (1, "left"),
+            (2, "right"),
+            (1, "right"),
+            (2, None)
+        ]
+
+        for steps, direction in path:
+            self.walkcase(steps)
+            if direction:
+                self.turn(direction)
