ce8266188d6b1cd4c41dd17fa6a79ea6b827fc8c
robin-r <robinrus42@gmail.com>
2025-05-22 10:46:50 +0200

    refactorisation de Moves.py avec circletime

--- Moves.py
+++ Moves.py
@@ -9,55 +9,13 @@ class Moves:
     def walkcase(self, case: int = 1):
         self.marty.walk(num_steps=case*7, start_foot='auto', turn=0, step_length=25, move_time=1500, blocking=None)
 
-    def sidestep(self, side: str, steps: int = 2,
-                 step_length: int = 35,
-                 move_time: int = 1000,
-                 blocking: Optional[bool] = None) -> bool:
-        try:
-            self.marty.sidestep(
-                side=side,
-                steps=steps,
-                step_length=step_length,
-                move_time=move_time,
-                blocking=blocking
-            )
-            print(f"Marty is sidestepping to the {side}!")
-            return True
-        except MartyConnectException as e:
-            print(f"Failed to make Marty sidestep: {e}")
-        except Exception as e:
-            print(f"Unexpected error while sidestepping: {e}")
-        return False
+    def sidecase(self, case: int = 1, side: str = "right"):
+        if side == "right":
+            self.marty.sidestep(side="right", steps=case*6, step_length=35, move_time=1000, blocking=True)
+        else:
+            self.marty.sidestep(side="left", steps=case*6, step_length=35, move_time=1000, blocking=True)
 
-    def arms(self, left_angle: int, right_angle: int, move_time: int,
-             blocking: Optional[bool] = None) -> bool:
-        try:
-            self.marty.arms(
-                left_angle=left_angle,
-                right_angle=right_angle,
-                move_time=move_time,
-                blocking=blocking
-            )
-            print(f"Marty's arms moved to left: {left_angle}, right: {right_angle}!")
-            return True
-        except MartyConnectException as e:
-            print(f"Failed to move Marty's arms: {e}")
-        except Exception as e:
-            print(f"Unexpected error while moving Marty's arms: {e}")
-        return False
-
-    def circle_dance(self, side: str = "right", move_time: int = 2500,
-                     blocking: Optional[bool] = None) -> bool:
-        try:
-            self.marty.circle_dance(
-                side=side,
-                move_time=move_time,
-                blocking=blocking
-            )
-            print(f"Marty circle_dance!")
-            return True
-        except MartyConnectException as e:
-            print(f"Failed to make Marty circle dance: {e}")
-        except Exception as e:
-            print(f"Unexpected error while circle dance: {e}")
-        return False
+    def circletime(self, time: int = 1):
+        for i in range(time):
+            self.marty.circle_dance(side="right", move_time=1000)
+            self.marty.circle_dance(side="left", move_time=1000)
