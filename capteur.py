ece94db97d7c9e5347e8134c242ddb1aaaf6c78f
Thomas Loctin <thomas.loctin@gmail.com>
2025-05-28 09:28:56 +0200

    Refactoring et amélioration du fichier capteur.py

--- capteur.py
+++ capteur.py
@@ -1,68 +1,75 @@
 from martypy import Marty
-import MartyController
-import mmap
 import time
 
-class Capteur:
-    def battery(Marty):
-        print("Marty's battery level:")
-        try:
-            battery = float(Marty.get_battery_remaining())
-        
-        except Exception as e:
-            print(f"Failed to read Marty batttery lvl: {e}")
 
-        print(f"{battery}%")
+def battery(Marty: Marty):
+    print("Marty's battery level:")
+    try:
+        battery = float(Marty.get_battery_remaining())
+        return battery
+    except Exception as e:
+        print(f"Failed to read Marty batttery lvl: {e}")
 
 
-    def distance(Marty):
-        print("Marty's distance:")
-        try:
-            distance = float(Marty.get_distance_sensor())
-        except Exception as e:
-            print(f"Failed to read Marty distance: {e}")
+def distance(Marty: Marty):
+    print("Marty's distance:")
+    try:
+        distance = float(Marty.get_distance_sensor())
+        return distance
+    except Exception as e:
+        print(f"Failed to read Marty distance: {e}")
 
-        print(distance)
 
+def obstacle(Marty: Marty):
+    try:
+        """obstaclebool_right = bool(Marty.foot_obstacle_sensed("right"))
+        obstaclebool_left = bool(Marty.foot_obstacle_sensed("left"))"""
+        obstaclevalue_right_original = float(
+            Marty.get_obstacle_sensor_reading("right"))
+        obstaclevalue_left_original = float(
+            Marty.get_obstacle_sensor_reading("left"))
+    except Exception as e:
+        print(f"Failed to read Marty obstacle: {e}")
 
-    def obsacle(Marty):
-        try:
-            """obstaclebool_right = bool(Marty.foot_obstacle_sensed("right"))
-            obstaclebool_left = bool(Marty.foot_obstacle_sensed("left"))"""
-            obstaclevalue_right_original = float(Marty.get_obstacle_sensor_reading("right"))
-            obstaclevalue_left_original = float(Marty.get_obstacle_sensor_reading("left"))
-        except Exception as e:
-            print(f"Failed to read Marty obstacle: {e}")
-
-        """ print(f"valeur bool droit: {obstaclebool_right}")
+    """ print(f"valeur bool droit: {obstaclebool_right}")
         print(f"valeur bool gauche: {obstaclebool_left}")"""
-        print(f"valeur droit: {obstaclevalue_right_original}")
-        print(f"valeur gauche: {obstaclevalue_left_original}")
+    print(f"valeur droit: {obstaclevalue_right_original}")
+    print(f"valeur gauche: {obstaclevalue_left_original}")
 
 
-    def  colorsensor(Marty):
-        try:
-            calibrate(Marty)
-        except Exception as e:
-            print(f"Failed to read Marty obstacle: {e}")
-"""
-        print("right: ", )
-        print("left: ", )"""
+def colorsensor(Marty: Marty):
+    try:
+        calibrate(Marty)
+    except Exception as e:
+        print(f"Failed to read Marty obstacle: {e}")
 
 
-def calibrate(Marty):
-    liste_couleur = ["bleu","rouge","vert","noir"]
+def get_color(Marty: Marty):
+    """This function reads the color sensor values for both left and right sensors."""
+    try:
+        valeur_couleur_gauche = int(
+            Marty.get_ground_sensor_reading("left"))
+        valeur_couleur_droite = int(
+            Marty.get_ground_sensor_reading("right"))
+        return (valeur_couleur_gauche, valeur_couleur_droite)
+    except Exception as e:
+        print(f"Failed to read Marty color sensor: {e}")
+
+
+def calibrate(Marty: Marty):
+    liste_couleur = ["bleu", "rouge", "vert", "noir"]
     fichier = open("calibration.txt", "w+")
-    for couleur in liste_couleur :
+    for couleur in liste_couleur:
         print(f"je calibre la couleur : {couleur} \nmettez la bonne couleur\n")
         time.sleep(1)
         print("en cours.")
         time.sleep(2)
         print("en cours..")
         time.sleep(3)
-        valeur_couleur_gauche =int(Marty.get_ground_sensor_reading (str("left")))
-        valeur_couleur_droite =int(Marty.get_ground_sensor_reading (str("right")))
+        valeur_couleur_gauche = int(
+            Marty.get_ground_sensor_reading(str("left")))
+        valeur_couleur_droite = int(
+            Marty.get_ground_sensor_reading(str("right")))
         print("scannez")
-        fichier.write(f"{couleur}:{valeur_couleur_gauche};{valeur_couleur_droite}\n")
-        
-    
+        fichier.write(
+            f"{couleur};{valeur_couleur_gauche};{valeur_couleur_droite}\n")
