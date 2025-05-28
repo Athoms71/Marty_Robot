37f7a1c2c2aabcac992866d24b396a00dd67067a
Thomas Loctin <thomas.loctin@gmail.com>
2025-05-28 09:54:20 +0200

    Adaptation aux différents types de fichiers analysés lors du read_file

--- file_management.py
+++ file_management.py
@@ -1,24 +1,22 @@
-import os
-
-
 def read_file(file_path: str):
-    """Read the contents of a file and returns a dictionnary containing pairs 'color' : '[left,right]'."""
+    """Read the contents of a file and returns a dictionnary containing pairs 'color' : '[left,right]' or a tuple (left,right)."""
     # Check if the file exists
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
     except FileNotFoundError:
         print(f"File {file_path} not found.")
-        return {}
-    couleurs = {}
     for line in lines:
         line = line.split(";")
-        couleurs[line[0]] = [int(line[1]), int(line[2])]
+        if (len(line) == 2):
+            couleurs = (line[0], line[1])
+        elif (len(line) == 3):
+            couleurs[line[0]] = [int(line[1]), int(line[2])]
     return couleurs
 
 
 def write_file(file_path: str, content: str):
-    """Write a string to a file with format 'color;left;right'."""
+    """Write a string to a file with format 'a;b;c'."""
     # Check if the file exists
     try:
         with open(file_path, 'a') as file:
