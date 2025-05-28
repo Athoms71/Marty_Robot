32a57e9c869cc5db15f86e4071a7cde666fdfe32
Thomas Loctin <thomas.loctin@gmail.com>
2025-05-28 09:56:22 +0200

    Refactoring du fichier main.py et création de la grille interne pour le parcours

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMessageBox
+# from PyQt5.QtWidgets import QApplication, QMessageBox
 from martypy import Marty
 import file_management as fm
-import MainWindow
-import Moves
+# import MainWindow
+from Moves import Moves
 
 
-def connect_Marty(ip_address: str = "192.168.0.101"):
+def connect_Marty(ip_address: str = "192.168.0.102"):
     """Connect to Marty using the given IP address."""
     try:
         marty = Marty(method="wifi", locator=ip_address)
@@ -14,15 +14,23 @@ def connect_Marty(ip_address: str = "192.168.0.101"):
             raise Exception("Marty is not connected")
         return marty
     except Exception as e:
-        QMessageBox.critical(None, "Connection Error", str(e))
+        # QMessageBox.critical(None, "Connection Error", str(e))
         sys.exit(1)
     return marty
 
 
 def create_grid():
     couleurs_theoriques = fm.read_file("calibration.txt")
+    cm = fm.read_file("couleur_parcours.txt")
     grid = [["" for _ in range(3)] for _ in range(3)]
     # TODO : Initialiser la grille avec les couleurs détectées
+    for i in range(3):
+        for j in range(3):
+            delta = 5
+            for color, value in couleurs_theoriques:
+                if (cm[0] > value[0]-delta and cm[0] < value[0]+delta) and (cm[1] > value[1]-delta and cm[1] < value[1]+delta):
+                    grid[i][j] = color
+    return grid
 
 
 def main():
@@ -31,17 +39,18 @@ def main():
 
     # Create the grid
     grid = create_grid()
+    print(grid)
 
     # Initialize QApplication
-    app = QApplication(sys.argv)
+    # app = QApplication(sys.argv)
 
     marty.get_ready()
     moves = Moves(marty)
-    window = MainWindow(moves)
-    window.show()
+    # window = MainWindow(moves)
+    # window.show()
 
     # Main loop
-    sys.exit(app.exec_())
+    # sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
