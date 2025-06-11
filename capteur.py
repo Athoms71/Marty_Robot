59d59c6a6378d7e6572e82d4b2db5bca79fa5ead
Emmanuel <Emmanuel_Peyronnet@etu.u-bourgogne.fr>
2025-06-11 16:53:23 +0200

    j enleve la classe capteur qui ne possède pas de sens

--- capteur.py
+++ capteur.py
@@ -3,14 +3,13 @@ import mmap
 import time
 import file_management as file_management
 
-class Capteur:
-    def battery(Marty):
-        print("Marty's battery level:")
-        try:
-            battery = float(Marty.get_battery_remaining())
-        
-        except Exception as e:
-            print(f"Failed to read Marty batttery lvl: {e}")
+def battery(Marty):
+    print("Marty's battery level:")
+    try:
+        battery = float(Marty.get_battery_remaining())
+    
+    except Exception as e:
+        print(f"Failed to read Marty batttery lvl: {e}")
 
 
 def distance(Marty: Marty):
@@ -39,31 +38,31 @@ def obstacle(Marty: Marty):
     print(f"valeur gauche: {obstaclevalue_left_original}")
 
 
-    def  colorsensor(Marty,liste_couleur):
-        try:
-            Capteur.calibrate(Marty,liste_couleur)
-            content = file_management.read_file("calibration.txt")
-            print(content)
-        except Exception as e:
-            print(f"Failed to read Marty colorsensor: {e}")
+def  colorsensor(Marty,liste_couleur):
+    try:
+        Capteur.calibrate(Marty,liste_couleur)
+        content = file_management.read_file("calibration.txt")
+        print(content)
+    except Exception as e:
+        print(f"Failed to read Marty colorsensor: {e}")
 
 
 
-    def calibrate(Marty,liste_couleur):
-        fichier = open("calibration.txt", "w")
-        for couleur in liste_couleur :
-            print(f"je calibre la couleur : {couleur} \nmettez la bonne couleur\n")
-            time.sleep(1)
-            print("en cours.")
-            time.sleep(1)
-            print("en cours..")
-            time.sleep(1)
-            valeur_couleur_gauche =int(Marty.get_ground_sensor_reading (str("left")))
-            valeur_couleur_droite =int(Marty.get_ground_sensor_reading (str("right")))
-            print("scannez")
-            fichier.write(f"{couleur};{valeur_couleur_gauche};{valeur_couleur_droite};\n")
-            print("c est ecrit")
-        fichier.close()
+def calibrate(Marty,liste_couleur):
+    fichier = open("calibration.txt", "w")
+    for couleur in liste_couleur :
+        print(f"je calibre la couleur : {couleur} \nmettez la bonne couleur\n")
+        time.sleep(1)
+        print("en cours.")
+        time.sleep(1)
+        print("en cours..")
+        time.sleep(1)
+        valeur_couleur_gauche =int(Marty.get_ground_sensor_reading (str("left")))
+        valeur_couleur_droite =int(Marty.get_ground_sensor_reading (str("right")))
+        print("scannez")
+        fichier.write(f"{couleur};{valeur_couleur_gauche};{valeur_couleur_droite};\n")
+        print("c est ecrit")
+    fichier.close()
 
         
     
