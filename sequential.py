db1cc5c928d4944b0f7eef019dc50182da299602
Thomas Loctin <thomas.loctin@gmail.com>
2025-06-11 15:10:23 +0200

    Documentation du fichier

--- sequential.py
+++ sequential.py
@@ -3,6 +3,9 @@ from MartyController import MartyController
 
 
 def play_dance(robot: MartyController, file_path: str):
+    """Lit les fichiers .dance en fonction de leur première ligne :\n
+        - Si ABS : parcourt le chemin absolu depuis le centre de la grille\n
+        - Si SEQ : parcourt le chemin relatif depuis la position de départ"""
     if file_path[-6:] == ".dance":
         movement = Moves()
         fichier = open(file_path, "r")
