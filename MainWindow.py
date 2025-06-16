33b0fece40b2ff5c4b1ef93d42bb2f65126c61fe
18132004 <amaury.gaborit17@gmail.com>
2025-06-16 10:43:38 +0200

    Add buttons and functionality for left and right arm movements in MainWindow

--- MainWindow.py
+++ MainWindow.py
@@ -92,6 +92,8 @@ class MainWindow(QWidget):
         btn_left = make_button('btn_left.png', 100, self.on_btn_left_clicked)
         btn_rotate_left = make_button('btn_rotate_left.png', 100, self.on_btn_rotate_left_clicked)
         btn_rotate_right = make_button('btn_rotate_right.png', 100, self.on_btn_rotate_right_clicked)
+        btn_right_arm = make_button('btn_right_arm.png', 100, self.on_btn_right_arm_clicked)
+        btn_left_arm = make_button('btn_left_arm.png', 100, self.on_btn_left_arm_clicked)
 
         btn_dominance_dance = make_button('btn_dominance_dance.png', 100, self.on_btn_dominance_dance_clicked)
         btn_circle_dance = make_button('btn_circle_dance.jpg', 100, self.on_btn_circle_dance_clicked)
@@ -141,6 +143,8 @@ class MainWindow(QWidget):
         grid_moves.addWidget(btn_left, 1, 0)
         grid_moves.addWidget(btn_rotate_left, 0, 0)
         grid_moves.addWidget(btn_rotate_right, 0, 2)
+        grid_moves.addWidget(btn_right_arm, 2, 2)
+        grid_moves.addWidget(btn_left_arm, 2, 0)
 
         grid_dances = QGridLayout()
         grid_dances.addWidget(btn_dominance_dance, 0, 0)
@@ -281,6 +285,20 @@ class MainWindow(QWidget):
     def on_btn_rotate_right_clicked(self):
         if self.mode == 0 and self.moves.marty and not self.moves.marty.is_moving():
             self.moves.turn("right")
+
+    def on_btn_right_arm_clicked(self):
+        if self.mode == 0 and self.moves.marty and not self.moves.marty.is_moving():
+            self.moves.get_marty().arms(left_angle=0, right_angle=135,
+                                        move_time=500, blocking=True)
+            self.moves.get_marty().arms(left_angle=0, right_angle=0,
+                                        move_time=500, blocking=True)
+
+    def on_btn_left_arm_clicked(self):
+        if self.mode == 0 and self.moves.marty and not self.moves.marty.is_moving():
+            self.moves.get_marty().arms(left_angle=135, right_angle=0,
+                                        move_time=500, blocking=True)
+            self.moves.get_marty().arms(left_angle=0, right_angle=0,
+                                        move_time=500, blocking=True)
 
     def keyPressEvent(self, event):
         # Ne rien faire si le focus est sur un champ texte (comme QLineEdit)
